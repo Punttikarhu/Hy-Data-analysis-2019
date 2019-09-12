cc06f1a64262b62fc53b90a85af92a80766d768b
Janne Saarinen <janne.saarinen@mail.com>
2019-09-12 13:55:46 +0300

    Add files via upload

---
+++ compliment.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def main():
+    # Enter you solution here
+    input_country = input("What country are you from? ")
+    print(f'I have heard that {input_country} is a beautiful country.')
+
+if __name__ == "__main__":
+    main()
